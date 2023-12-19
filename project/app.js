95946ccbdba60e65b80c5510a4f71545d13c6346
shafirio6 <144016413+shafirio6@users.noreply.github.com>
2023-12-19 15:36:13 +0100

    ...

---
+++ project/app.js
@@ -0,0 +1,17 @@
+
+
+function click() {
+    
+}
+
+function reappear(){
+    document.getElementById("getback").style.display = "unset";
+}
+
+function reappear2(){
+    document.getElementById("getback2").style.display = "unset";
+}
+
+function reappear3(){
+    document.getElementById("getback3").style.display = "unset";
+}
